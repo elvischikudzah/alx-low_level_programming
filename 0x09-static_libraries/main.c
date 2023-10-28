6d59382460e07d0ddf50eb222e8a5fec358cd221
elvischikudzah <elvischikudza@gmail.com>
2023-10-28 05:57:46 -0700

    main.c created

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - Entry point of the program
+*
+* Description: This function prints a quote to the standard output using the
+* _puts function.
+*
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
